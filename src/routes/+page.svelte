648df6ffbf2a5e3eb7d12e5cbe15aef8f10d0fde
Emona Santiago <emonasantiago1207@gmail.com>
2022-12-18 14:20:34 +0100

    pop-art title

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<body>
+  <span>CMD MIDTERM</span>
+  <nav class="navigation">
+    <a href="" class="">Beoogde leerresultaten</a>
+    <a href="" class="">Onderwijsleeromgeving</a>
+    <a href="" class="">Toetsing</a>
+    <a href="" class="">Gerealiseerde leerresultaten</a>
+  </nav>
+</body>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@900&display=swap");
+  body {
+    background-color: yellow;
+  }
+
+  nav a {
+    color: black;
+    text-decoration: none;
+  }
+
+  span {
+    background: yellow;
+    font-family: "Raleway", sans-serif;
+  }
+
+  span {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 120px;
+    letter-spacing: 0.1em;
+    -webkit-text-fill-color: transparent;
+    -webkit-text-stroke-width: 3px;
+    -webkit-text-stroke-color: white;
+    text-shadow: 8px 8px #530988, 20px 20px #000000;
+  }
+</style>
